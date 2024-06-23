42f9186b0f04ad856384834f80025d0570a249cd
Lucas Alves <lucasalves_taz@hotmail.com>
2024-06-23 01:13:40 -0300

    up playscores_api.py

--- SRC/playscores_api.py
+++ SRC/playscores_api.py
@@ -1,11 +1,12 @@
 import requests
 
-url = "https://radarw.com/api/ListApi1.php"
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
-}
 
+def jogos():
+    url = "https://radarw.com/api/ListApi1.php"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+    }
 
-content = requests.get(url, headers=headers)
+    content = requests.get(url, headers=headers)
 
-print(content)
+    return print(content)
